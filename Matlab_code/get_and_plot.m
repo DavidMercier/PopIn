e81f237b04b8437d9f659ba773f005ef77e31446
david mercier <d.mercier@mpie.de>
2015-09-02 10:46:13 +0200

    debugging of the Excel file loading with empty or invalid sheets...

--- Matlab_code/get_and_plot.m
+++ Matlab_code/get_and_plot.m
@@ -20,54 +20,52 @@ else
     
     guidata(gcf, gui);
     %% Import data
-    for ii_sheet = 1:1:length(gui.data_xls.sheets_xls)
+    for ii_sheet = 1:1:gui.data_xls.sheets_xls_notEmpty
         gui = guidata(gcf);
         if gui.flag.flag_cleaned_data
-            if ~isempty(gui.data(ii_sheet).data_h)
-                if ishandle(gui.handles.h_waitbar)
-                    waitbar(ii_sheet / length(gui.data_xls.sheets_xls), gui.handles.h_waitbar);
+            if ishandle(gui.handles.h_waitbar)
+                waitbar(ii_sheet / gui.data_xls.sheets_xls_notEmpty, gui.handles.h_waitbar);
+            end
+            
+            % Cleaning and cropping of data from min and max depth set by user
+            clean_data(ii_sheet);
+            gui = guidata(gcf); guidata(gcf, gui);
+            
+            if gui.flag.flag_cleaned_data == 1
+                %% Calculation of the number of pop-in
+                % Determination of the maximum displacement from cleaned and cropped data
+                for uu = gui.settings.max_bound_h-2:1:gui.settings.max_bound_h
+                    if rem(uu, 1) == 0
+                        umax(ii_sheet) = uu;
+                    end
                 end
                 
-                % Cleaning and cropping of data from min and max depth set by user
-                clean_data(ii_sheet);
-                gui = guidata(gcf); guidata(gcf, gui);
+                % Calculation of 1st derivative
+                gui.data(ii_sheet).data_dh_cleaned = diff(gui.data(ii_sheet).data_h_cleaned);
+                gui.data(ii_sheet).data_dL_cleaned = diff(gui.data(ii_sheet).data_L_cleaned);
+                gui.data(ii_sheet).data_dh_cleaned(length(gui.data(ii_sheet).data_dh_cleaned)+1) = gui.data(ii_sheet).data_dh_cleaned(length(gui.data(ii_sheet).data_dh_cleaned));
+                gui.data(ii_sheet).data_dL_cleaned(length(gui.data(ii_sheet).data_dL_cleaned)+1) = gui.data(ii_sheet).data_dL_cleaned(length(gui.data(ii_sheet).data_dL_cleaned));
                 
-                if gui.flag.flag_cleaned_data == 1
-                    %% Calculation of the number of pop-in
-                    % Determination of the maximum displacement from cleaned and cropped data
-                    for uu = gui.settings.max_bound_h-2:1:gui.settings.max_bound_h
-                        if rem(uu, 1) == 0
-                            umax(ii_sheet) = uu;
-                        end
-                    end
-                    
-                    % Calculation of 1st derivative
-                    gui.data(ii_sheet).data_dh_cleaned = diff(gui.data(ii_sheet).data_h_cleaned);
-                    gui.data(ii_sheet).data_dL_cleaned = diff(gui.data(ii_sheet).data_L_cleaned);
-                    gui.data(ii_sheet).data_dh_cleaned(length(gui.data(ii_sheet).data_dh_cleaned)+1) = gui.data(ii_sheet).data_dh_cleaned(length(gui.data(ii_sheet).data_dh_cleaned));
-                    gui.data(ii_sheet).data_dL_cleaned(length(gui.data(ii_sheet).data_dL_cleaned)+1) = gui.data(ii_sheet).data_dL_cleaned(length(gui.data(ii_sheet).data_dL_cleaned));
-                    
-                    % Calculation of 2nd derivative
-                    gui.data(ii_sheet).data_ddh_cleaned = diff(gui.data(ii_sheet).data_dh_cleaned);
-                    gui.data(ii_sheet).data_ddL_cleaned = diff(gui.data(ii_sheet).data_dL_cleaned);
-                    gui.data(ii_sheet).data_ddh_cleaned(length(gui.data(ii_sheet).data_ddh_cleaned)+1) = gui.data(ii_sheet).data_ddh_cleaned(length(gui.data(ii_sheet).data_ddh_cleaned));
-                    gui.data(ii_sheet).data_ddL_cleaned(length(gui.data(ii_sheet).data_ddL_cleaned)+1) = gui.data(ii_sheet).data_ddL_cleaned(length(gui.data(ii_sheet).data_ddL_cleaned));
-                    
-                    delta_data = max(gui.data(ii_sheet).data_ddh_cleaned(:)) - mean(gui.data(ii_sheet).data_ddh_cleaned(:));
-                    
-                    [maxpeak, minpeak] = peakdet(gui.data(ii_sheet).data_ddh_cleaned(:), delta_data);
-                    
-                    gui.data(ii_sheet).num_popin_int = maxpeak(:, 1);
-                    
-                    if gui.data(ii_sheet).num_popin_int == -1;
-                        delete(gui.handles.h_waitbar);
-                        warndlg('No pop-in detected !');
-                        return;
-                    end
-                    guidata(gcf, gui);
-                else
+                % Calculation of 2nd derivative
+                gui.data(ii_sheet).data_ddh_cleaned = diff(gui.data(ii_sheet).data_dh_cleaned);
+                gui.data(ii_sheet).data_ddL_cleaned = diff(gui.data(ii_sheet).data_dL_cleaned);
+                gui.data(ii_sheet).data_ddh_cleaned(length(gui.data(ii_sheet).data_ddh_cleaned)+1) = gui.data(ii_sheet).data_ddh_cleaned(length(gui.data(ii_sheet).data_ddh_cleaned));
+                gui.data(ii_sheet).data_ddL_cleaned(length(gui.data(ii_sheet).data_ddL_cleaned)+1) = gui.data(ii_sheet).data_ddL_cleaned(length(gui.data(ii_sheet).data_ddL_cleaned));
+                
+                delta_data = max(gui.data(ii_sheet).data_ddh_cleaned(:)) - mean(gui.data(ii_sheet).data_ddh_cleaned(:));
+                
+                [maxpeak, minpeak] = peakdet(gui.data(ii_sheet).data_ddh_cleaned(:), delta_data);
+                
+                gui.data(ii_sheet).num_popin_int = maxpeak(:, 1);
+                
+                if gui.data(ii_sheet).num_popin_int == -1;
                     delete(gui.handles.h_waitbar);
+                    warndlg('No pop-in detected !');
+                    return;
                 end
+                guidata(gcf, gui);
+            else
+                delete(gui.handles.h_waitbar);
             end
         end
     end
@@ -76,50 +74,48 @@ else
         delete(gui.handles.h_waitbar);
         
         %% Calculation of probability for Weibull cumulative distribution function
-        gui.results(1, 1).prob = (1/length(gui.data_xls.sheets_xls));
-        for ii = 1:(length(gui.data_xls.sheets_xls)-1),
+        gui.results(1, 1).prob = (1/gui.data_xls.sheets_xls_notEmpty);
+        for ii = 1:(gui.data_xls.sheets_xls_notEmpty-1),
             gui.results(ii+1, 1).prob = (((gui.results(ii, 1).prob * ...
-                (length(gui.data_xls.sheets_xls)+1))+1)/(length(gui.data_xls.sheets_xls)+1));
+                (gui.data_xls.sheets_xls_notEmpty+1))+1)/(gui.data_xls.sheets_xls_notEmpty+1));
         end
         
         %% Sum of data for statistic analysis and plot of pop-in distribution + fit
         gui.handles.h_waitbar = waitbar(0, 'Plots in progress...');
         
-        for ii_sheet = 1:1:length(gui.data_xls.sheets_xls)
-            waitbar(ii_sheet / length(gui.data_xls.sheets_xls));
+        for ii_sheet = 1:1:gui.data_xls.sheets_xls_notEmpty
+            waitbar(ii_sheet / gui.data_xls.sheets_xls_notEmpty);
             
             ind_popin_all = gui.data(ii_sheet).num_popin_int(:);
-            if ~isempty(gui.data(ii_sheet).data_h)
-                if gui.settings.set_popin == 1
-                    if isempty(ind_popin_all)
-                        ind_popin_all(1) = gui.settings.max_bound_h_init;
-                    end
-                    ind_popin = ind_popin_all(1);
-                    
-                else
-                    if length(ind_popin_all) == 1
-                        ind_popin_all(2) = gui.settings.max_bound_h_init;
-                    end
-                    ind_popin = ind_popin_all(2);
-                    
+            if gui.settings.set_popin == 1
+                if isempty(ind_popin_all)
+                    ind_popin_all(1) = gui.settings.max_bound_h_init;
                 end
+                ind_popin = ind_popin_all(1);
                 
-                if gui.settings.value_crit_param == 1
-                    gui.data(ii_sheet).sum_L = ...
-                        gui.data(ii_sheet).data_L_cleaned(ind_popin);
-                    
-                    gui.data(ii_sheet).mean_sum_L = ...
-                        mean(gui.data(ii_sheet).sum_L);
-                    
-                elseif gui.settings.value_crit_param == 2
-                    gui.data(ii_sheet).sum_h = ...
-                        gui.data(ii_sheet).data_h_cleaned(ind_popin);
-                    
-                    gui.data(ii_sheet).mean_sum_h = ...
-                        mean(gui.data(ii_sheet).sum_h);
+            else
+                if length(ind_popin_all) == 1
+                    ind_popin_all(2) = gui.settings.max_bound_h_init;
                 end
-                guidata(gcf, gui);
+                ind_popin = ind_popin_all(2);
+                
             end
+            
+            if gui.settings.value_crit_param == 1
+                gui.data(ii_sheet).sum_L = ...
+                    gui.data(ii_sheet).data_L_cleaned(ind_popin);
+                
+                gui.data(ii_sheet).mean_sum_L = ...
+                    mean(gui.data(ii_sheet).sum_L);
+                
+            elseif gui.settings.value_crit_param == 2
+                gui.data(ii_sheet).sum_h = ...
+                    gui.data(ii_sheet).data_h_cleaned(ind_popin);
+                
+                gui.data(ii_sheet).mean_sum_h = ...
+                    mean(gui.data(ii_sheet).sum_h);
+            end
+            guidata(gcf, gui);
         end
         delete(gui.handles.h_waitbar);
         
--- Matlab_code/util/openfile.m
+++ Matlab_code/util/openfile.m
@@ -10,6 +10,11 @@ gui.data_xls.filename_data = filename_data;
 gui.data_xls.pathname_data = pathname_data;
 fullname_data = fullfile(pathname_data, filename_data);
 
+% Get the number of sheets in the .xls file and the status of the .xls file
+[status_xls, sheets_xls] = xlsfinfo(fullname_data);
+gui.data_xls.status_xls = status_xls;
+gui.data_xls.sheets_xls = sheets_xls;
+
 %% Handle canceled file selection
 if filename_data == 0
     filename_data = '';
@@ -35,9 +40,14 @@ if strcmp (ext, '.nul') == 1
     errorLoadingData;
     
 elseif strcmp (ext, '.xls') == 1 || strcmp (ext, '.xlsx') == 1
-    sheet = 1;
-    [gui.data, txt] = xlsread(fullname_data, sheet);
-    str_endsegment = txt(:,1); %limite
+    
+    for ii_sheet = 1:1:length(sheets_xls)
+        [gui.data, txt] = xlsread(fullname_data, ii_sheet);
+        str_endsegment = txt(:,1); %limite
+        if ~isempty(str_endsegment)
+            break
+        end
+    end
     
     if isempty(str_endsegment)
         helpdlg('No segment found', 'Info');
@@ -50,66 +60,88 @@ elseif strcmp (ext, '.xls') == 1 || strcmp (ext, '.xlsx') == 1
         [s__endsegment, v__endsegment] = listdlg('PromptString', 'Select an end segment:', ...
             'SelectionMode', 'single',...
             'ListString', str_endsegment_true);
-        
-        % Get the number of sheets in the .xls file
-        [status_xls, sheets_xls] = xlsfinfo(fullname_data);
-        gui.data_xls.status_xls = status_xls;
-        gui.data_xls.sheets_xls = sheets_xls;
     end
-    guidata(gcf, gui);
     
     %% Import data from .xls file
     
-    gui.handles.h_waitbar = waitbar(0, 'Import of data in progress...');
+    %raw_str_endsegment = cell(length(sheets_xls),1);
+    y_index = NaN(length(sheets_xls), 1);
     
-    % Preallocation and initialization
-    gui.data = struct();
-    min_data_h = NaN(length(sheets_xls));
-    max_data_h = NaN(length(sheets_xls));
-    min_data_L = NaN(length(sheets_xls));
-    max_data_L = NaN(length(sheets_xls));
-    
+    % Check and remove empty sheets
+    notEmpty_data = NaN(length(sheets_xls));
+    gui.handles.h_waitbar_1 = waitbar(0, 'Check Excel file (remove empty sheets)...'); % Don't move into the for loop
     for ii_sheet = 1:1:length(sheets_xls)
-        waitbar(ii_sheet / length(sheets_xls), gui.handles.h_waitbar);
+        waitbar(ii_sheet / length(sheets_xls), gui.handles.h_waitbar_1);
         
         [data, txt] = xlsread(fullname_data, ii_sheet);
         
         if ~isempty(txt)
             raw_str_endsegment = txt(:,1);
             
-            % Set the y index to crop data in function of chosen segment
-            y_index = find(strcmp(raw_str_endsegment, ...
-                str_endsegment_true(s__endsegment)) == 1);
-            
-            if ~isempty(y_index)
-                data_index = sprintf('%c%d:%c%d', 'B', 1, 'C', y_index);
-                
-                [data_cropped, txt_cropped] = ...
-                    xlsread(fullname_data, ii_sheet, data_index);
-                
-                % Import data
-                gui.data(ii_sheet).data_h = data_cropped(:, 1);
-                gui.data(ii_sheet).data_L = data_cropped(:, 2);
-                min_data_h(ii_sheet) = round(min(gui.data(ii_sheet).data_h));
-                max_data_h(ii_sheet) = round(max(gui.data(ii_sheet).data_h));
-                min_data_L(ii_sheet) = min(gui.data(ii_sheet).data_L);
-                max_data_L(ii_sheet) = max(gui.data(ii_sheet).data_L);
-                
+            if ~isempty(raw_str_endsegment)
+                % Set the y index to crop data in function of chosen segment
+                raw_index = find(strcmp(raw_str_endsegment, ...
+                    str_endsegment_true(s__endsegment)) == 1);
+                if ~isempty(raw_index)
+                    y_index(ii_sheet) = raw_index;
+                else
+                    y_index(ii_sheet) = [];
+                end
             else
+                y_index(ii_sheet) = [];
+            end
+            
+            if ~isempty(y_index(ii_sheet)) && ~isnan(y_index(ii_sheet))
+                notEmpty_data(ii_sheet) = ii_sheet;
+            else
+                notEmpty_data(ii_sheet) = [];
                 display(strcat('Missing segment for the sheet number_', ...
                     num2str(ii_sheet)));
-                gui.data(ii_sheet).data_h = [];
-                gui.data(ii_sheet).data_L = [];
             end
             
         else
             display('Empty Excel sheet !');
-            gui.data(ii_sheet).data_h = [];
-            gui.data(ii_sheet).data_L = [];
+            notEmpty_data(ii_sheet) = [];
         end
     end
+    delete(gui.handles.h_waitbar_1);
     
-    delete(gui.handles.h_waitbar);
+    notEmpty_data(isnan(notEmpty_data)) = [];
+    gui.data_xls.sheets_xls_notEmpty = length(notEmpty_data);
+    
+    % Preallocation and initialization
+    gui.data = struct();
+    gui.results = struct();
+    gui.cumulativeFunction = struct();
+    gui.Hertz = struct();
+    
+    min_data_h = NaN(gui.data_xls.sheets_xls_notEmpty);
+    max_data_h = NaN(gui.data_xls.sheets_xls_notEmpty);
+    min_data_L = NaN(gui.data_xls.sheets_xls_notEmpty);
+    max_data_L = NaN(gui.data_xls.sheets_xls_notEmpty);
+    
+    % Import data
+    gui.handles.h_waitbar_2 = waitbar(0, 'Import of data in progress...');
+    for ii_sheet = 1:1:gui.data_xls.sheets_xls_notEmpty
+        waitbar(ii_sheet / gui.data_xls.sheets_xls_notEmpty, gui.handles.h_waitbar_2); % Don't move into the for loop
+        
+        ii_sheet2read = notEmpty_data(ii_sheet);
+
+        data_index = sprintf('%c%d:%c%d', 'B', 1, 'C', y_index(ii_sheet2read));
+        
+        [data_cropped, txt_cropped] = ...
+            xlsread(fullname_data, ii_sheet2read, data_index);
+        
+        gui.data(ii_sheet).data_h = data_cropped(:, 1);
+        gui.data(ii_sheet).data_L = data_cropped(:, 2);
+        min_data_h(ii_sheet) = round(min(gui.data(ii_sheet).data_h));
+        max_data_h(ii_sheet) = round(max(gui.data(ii_sheet).data_h));
+        min_data_L(ii_sheet) = min(gui.data(ii_sheet).data_L);
+        max_data_L(ii_sheet) = max(gui.data(ii_sheet).data_L);
+        
+    end
+    delete(gui.handles.h_waitbar_2);
+    
     gui.settings.min_bound_h = max(min_data_h(:));
     gui.settings.max_bound_h = min(max_data_h(:));
     gui.settings.min_bound_h_init = max(min_data_h(:));
@@ -130,7 +162,7 @@ elseif strcmp (ext, '.xls') == 1 || strcmp (ext, '.xlsx') == 1
     gui.flag.flag_data = 1;
     gui.flag.flag_cleaned_data = 1;
     set(gui.handles.run_calc, 'BackgroundColor', [0 204/256 0]);
-    guidata(gcf, gui);
+    guidata(gcf, gui); % Don't move because of the following helpdlg !
     
     % Message to the user
     helpdlg('Data imported ! Set parameters for statistic analysis and run the calculations...', 'Info');
